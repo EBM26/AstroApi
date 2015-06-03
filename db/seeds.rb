cdf700e45ca5f34aa2ed8b4391ce7d18a4304724
Eyal Benmoshe <ebm2626@yahoo.com>
2015-06-03 00:37:16 -0700

    added Taurus

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
 signs = Sign.create([
-    {name: "Aries", date: "March 21 - April 20", element: "Fire", stage: "Cardinal", planet: "Mars", appearance: "Aries usually has a taller than average height; rarely short. He is well balanced at both upper and lower body sections so that if his shoulders are big, so are his legs. His face is made of short, straight, clear-cut, distinctive lines. He usually has strong eyebrows. His most distinctive features are that his body is always upright, never slouching, and that his upper body and head are always leaning forward, as if rushing somewhere.", karmic_stage: "New Born", strengths: "Aries’ strong traits are: wonder, innocence, blind faith, bravery, energetic, diligence, and enthusiasm.", weaknesses: "Aries' weaknesses are: selfishness, inconsideration, over-aggressiveness, hasty actions provoked by urges without calculating the consequences, and stubbornness.", hobbies: "Aries enjoys being active and showing off his superiority to all. Therefore, Aries loves sports, especially the kinds that are more individual rather than team sports, such as running or jumping. He also likes the martial since he is so aware of his body and his movement. His natural aggressiveness finds self-expression within the martial arts. He loves fast cars.", color: "red", stone: "ruby", metal: "iron", related_body_part: "head" }
 
+    {
+       name: "Aries", 
+       date: "March 21 - April 20",
+       element: "Fire",
+       stage: "Cardinal",
+       planet: "Mars", 
+       appearance: "Aries usually has a taller than average height; rarely short. He is well balanced at both upper and lower body sections so that if his shoulders are big, so are his legs. His face is made of short, straight, clear-cut, distinctive lines. He usually has strong eyebrows. His most distinctive features are that his body is always upright, never slouching, and that his upper body and head are always leaning forward, as if rushing somewhere",
+       karmic_stage: "New Born", 
+       strengths: "Aries’ strong traits are: wonder, innocence, blind faith, bravery, energetic, diligence, and enthusiasm.", 
+       weaknesses: "Aries' weaknesses are: selfishness, inconsideration, over-aggressiveness, hasty actions provoked by urges without calculating the consequences, and stubbornness.", 
+       hobbies: "Aries enjoys being active and showing off his superiority to all. Therefore, Aries loves sports, especially the kinds that are more individual rather than team sports, such as running or jumping. He also likes the martial since he is so aware of his body and his movement. His natural aggressiveness finds self-expression within the martial arts. He loves fast cars.", 
+       color: "red", 
+       stone: "ruby", 
+       metal: "iron", 
+       related_body_part: "head" 
+     }
 
+    {
+       name: "Taurus",
+       date: "April 21 - May 21",
+       element: "Earth",
+       stage: "Fixed",
+       planet: "Venus",
+       appearance: "Taurus has a medium to short height, rarely tall. His body is unusually rectangular, broad, and solid built. The upper half is much broader and stronger than the lower half. Strong, short neck. Broad face. Strong, distinguished jaws. Long vertical ears that tend to stick out from his head like a bull.",
+       karmic_stage: "Baby",
+       strengths: "Taurus' strengths are: determination, persistence, patience, efficiency, stability, reliability, rationality, sensuality",
+       weaknesses: "Taurus' weaknesses are: stubbornness, unwilling to change, narrow-mindedness, controlling, gluttony",
+       hobbies: "Taurus' earthliness, sensuality, and calm nature cause him to love activities of that manner. Gardening, cooking, and music are Taurus' favorite past-times and ways to relax. He also really loves food.",
+       color: "green", 
+       stone: "emerald",
+       metal: "copper",
+       related_body_part: "throat"
+     }    
 
 
   ])
